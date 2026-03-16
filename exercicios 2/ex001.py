c49b083ee3839f390de098ff4b4e678c7948a524
Cazé <carloshenrik0909@gmail.com>
2026-03-16 14:11:16 -0300

    exercicio 1

---
+++ exercicios 2/ex001.py
@@ -0,0 +1,11 @@
+aluno = {
+    'nome' :'Cazé',
+    'turma' : '3 E',
+    'media' : 7.0
+}
+
+nome = aluno['nome']
+turma = aluno['turma']
+media = aluno['media']
+
+print(f"Aluno: {nome} | Turma: {turma} | Média: {media}")
