554cb3df7dded1967e7b3f0d5d9f5c1de010e29f
Cazé <carloshenrik0909@gmail.com>
2026-03-16 14:28:57 -0300

    aula de dicionarios

---
+++ dicionario/ex002.py
@@ -0,0 +1,11 @@
+frase = 'a rata roeu a roupa do rei de roma'
+palavras = frase.split()
+contagem = {}
+
+for palavra in palavras:
+    if palavra in contagem:
+        contagem[palavra] += 1
+    else:
+        contagem[palavra] = 1
+        
+print(contagem)
