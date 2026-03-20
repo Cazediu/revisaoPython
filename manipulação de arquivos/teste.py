235c944c672185750965a0ea1decf16c0fef394f
Cazé Portela <carloshenrik0909@gmail.com>
2026-03-20 13:41:11 -0300

    lendo o arquivo

--- manipulação de arquivos/teste.py
+++ manipulação de arquivos/teste.py
@@ -1,9 +1,20 @@
+#Cria um arquivo e adiciona linhas 
 with open('manipulação de arquivos/teste.txt','w', encoding ='utf-8') as arquivo:
 
     arquivo.write("Primeira Linha\n")
     arquivo.write("Segunda Linha\n")
 
-with open('teste.txt','a', encoding='utf-8') as arquivo:
+
+
+#Adiciona linhas no .txt depois
+with open('manipulação de arquivos/teste.txt','a', encoding='utf-8') as arquivo:
 
     arquivo.write("Terceira Linha\n")
     arquivo.write("Quarta Linha\n")
+
+
+
+#Lê o arquivo inteiro em str
+with open('manipulação de arquivos/teste.txt','r', encoding='utf-8') as arquivo:
+    conteudo = arquivo.read()
+    print(conteudo)
