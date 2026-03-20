e27228ec9fe0144cfd4960043e1fea522e386a3a
Cazé Portela <carloshenrik0909@gmail.com>
2026-03-20 13:36:57 -0300

    adionando linha

--- manipulação de arquivos/teste.py
+++ manipulação de arquivos/teste.py
@@ -1,3 +1,9 @@
-with open("teste.txt","w", enconding="utf-8") as arquivo:
+with open('manipulação de arquivos/teste.txt','w', encoding ='utf-8') as arquivo:
+
     arquivo.write("Primeira Linha\n")
     arquivo.write("Segunda Linha\n")
+
+with open('teste.txt','a', encoding='utf-8') as arquivo:
+
+    arquivo.write("Terceira Linha\n")
+    arquivo.write("Quarta Linha\n")
